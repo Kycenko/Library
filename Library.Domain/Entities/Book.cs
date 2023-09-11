1d0acd8e3764a4729e6dea84ac26afcd2f9d1ed0
Kycenko <lagunovich.vladislav2003@gmail.com>
2023-09-11 20:22:36 +0300

    Добавьте файлы проекта.

---
+++ Library.Domain/Entities/Book.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Library.Domain.Entities
+{
+    public class Book 
+    {
+        public Guid BookId { get; set; }
+        public string? BookName { get; set; }
+        public string? BookAuthor { get; set; }
+        public int PublishYear { get; set; }
+        public string? Category { get; set; }
+    
+    }
+}
---
+++ Library.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Library.Domain.Entities;
+
+public class User 
+{
+
+	public Guid Id { get; set; }
+	public string? UserName { get; set; }
+	public string? UserPassword { get; set; }
+	public string? UserEmail { get; set; }
+	public DateTime? CreatedDate { get; set; }
+	public DateTime? UpdatedDate { get; set; }
+}
